d9a5edb1ff33fa35149137d650940261ff56188c
Danil Knyazev <knyazevdanik55@gmail.com>
2024-10-31 00:25:00 +0300

    change headers (cors error)

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: '*'
+    origin: process.env.CLIENT_URL
 }))
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
